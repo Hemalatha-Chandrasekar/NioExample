bc2d8ed1c3fa614e00f236f29831444fa225ffe0
Hemalatha-Chandrasekar <hema.alliswell@gmail.com>
2024-12-23 14:20:11 -0600

    Commit

--- src/NioExampleTwo.java
+++ src/NioExampleTwo.java
@@ -1,4 +1,36 @@
-package PACKAGE_NAME;
+import java.io.*;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.channels.WritableByteChannel;
+public class NioExampleTwo{
+    public static void main(String[] args) throws IOException {
+        // TODO Auto-generated method stub
 
-public class NioExampleTwo {
+        //Input files
+        String[] inputFiles = {"/Users/hema/Downloads/file1.txt", "/Users/hema/Downloads/file2.txt"};
+
+        //Files contents will be written in these files
+        String outputFile = "/Users/hema/Downloads/file3.txt";
+        //Get channel for output file
+
+        FileOutputStream fos = new FileOutputStream(outputFile);
+        WritableByteChannel targetChannel = fos.getChannel();
+        for (int i = 0; i < inputFiles.length; i++)
+        {
+            //Get channel for input files
+            FileInputStream fis = new FileInputStream(inputFiles[i]);
+            FileChannel inputChannel = fis.getChannel();
+
+            //Transfer data from input channel to output channel
+            inputChannel.transferTo(0, inputChannel.size(), targetChannel);
+
+            //close the input channel
+            inputChannel.close();
+            fis.close();
+        }
+
+        //finally close the target channel
+        targetChannel.close();
+        fos.close();
+    }
 }
